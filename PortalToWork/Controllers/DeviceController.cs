0951c28b24ecf4b464a05d8b4c137d0dc392d83a
Levi Zitting <me@levizitting.com>
2019-11-02 20:47:11 -0500

    Remove device repository

--- PortalToWork/Controllers/DeviceController.cs
+++ PortalToWork/Controllers/DeviceController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PortalToWork.Data;
 using PortalToWork.Models;
-using PortalToWork.Repository;
 
 namespace PortalToWork.Controllers
 {
